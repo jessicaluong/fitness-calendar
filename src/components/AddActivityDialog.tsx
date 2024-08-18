73c73a0259a004380ed7580bce8e1d918bbebe9a
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-08-18 14:26:13 -0700

    Implement Add Exercise in AddActivityDialog

--- src/components/AddActivityDialog.tsx
+++ src/components/AddActivityDialog.tsx
@@ -27,25 +27,29 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { CategoryData, CATEGORIES } from "@/lib/types";
+import { CategoryData, CATEGORIES, Exercise, Activity } from "@/lib/types";
+import { useState } from "react";
+
+const exerciseSchema = z.object({
+  name: z.string().min(1, "Exercise name is required"),
+  sets: z.coerce.number(),
+  reps: z.coerce.number(),
+  weight: z.coerce.number(),
+});
 
 const formSchema = z.object({
   activity: z.string().min(1, {
-    message: "Activity name is required.",
+    message: "Required",
   }),
-  minutes: z
-    .string()
-    .min(1, {
-      message: "Minutes is required.",
-    })
-    .refine((val) => !isNaN(parseInt(val)), {
+  minutes: z.coerce
+    .number({
       message: "Minutes must be a valid number.",
     })
-    .transform((val) => parseInt(val))
-    .refine((val) => val > 0, {
-      message: "Minutes must be at least 0.",
+    .min(1, {
+      message: "Minutes must greater than 0.",
     }),
   category: z.enum(CATEGORIES),
+  exercises: z.array(exerciseSchema),
 });
 
 type AddActivityDialogProps = {
@@ -55,33 +59,51 @@ type AddActivityDialogProps = {
 export function AddActivityDialog({
   handleAddActivity,
 }: AddActivityDialogProps) {
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       activity: "",
-      minutes: undefined,
+      minutes: 30,
       category: undefined,
+      exercises: [],
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    const activity: Activity = {
+      minutes: values.minutes,
+      name: values.activity,
+    };
+
+    if (values.exercises && values.exercises.length > 0) {
+      activity.exercises = values.exercises;
+    }
+
     const newActivity: CategoryData = {
       category: values.category,
-      activities: [{ minutes: values.minutes, name: values.activity }],
+      activities: [activity],
     };
+
     handleAddActivity(newActivity);
     console.log(values);
+    // TODO: close dialog after submit
   }
+
+  const exerciseFields = [
+    { name: "name", label: "Exercise name", placeholder: "Bench Press" },
+    { name: "sets", label: "Sets", placeholder: "3" },
+    { name: "reps", label: "Reps", placeholder: "10" },
+    { name: "weight", label: "Weight (in lbs)", placeholder: "135" },
+  ];
 
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button>Add</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-h-screen sm:max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Add New Activity</DialogTitle>
           <DialogDescription />
@@ -96,13 +118,13 @@ export function AddActivityDialog({
               name="activity"
               render={({ field }) => (
                 <FormItem className="flex flex-col">
-                  <div className="grid grid-cols-4 md:grid-cols-3 items-center gap-4">
+                  <div className="grid grid-cols-4 sm:grid-cols-3 items-center gap-4">
                     <FormLabel className="text-right">Activity Name</FormLabel>
-                    <FormControl className="col-span-3 md:col-span-2">
+                    <FormControl className="col-span-3 sm:col-span-2">
                       <Input placeholder="walking" {...field} />
                     </FormControl>
                   </div>
-                  <FormMessage className="mt-1 ml-[25%] md:ml-[33%]" />
+                  <FormMessage className="mt-1 ml-[27%] sm:ml-[35%]" />
                 </FormItem>
               )}
             />
@@ -111,15 +133,15 @@ export function AddActivityDialog({
               name="minutes"
               render={({ field }) => (
                 <FormItem className="flex flex-col">
-                  <div className="grid grid-cols-4 md:grid-cols-3 items-center gap-4">
+                  <div className="grid grid-cols-4 sm:grid-cols-3 items-center gap-4">
                     <FormLabel className="text-right">
                       Minutes Performed
                     </FormLabel>
-                    <FormControl className="col-span-3 md:col-span-2">
-                      <Input placeholder="30" {...field} />
+                    <FormControl className="col-span-3 sm:col-span-2">
+                      <Input {...field} />
                     </FormControl>
                   </div>
-                  <FormMessage className="mt-1 ml-[25%] md:ml-[33%]" />
+                  <FormMessage className="mt-1 ml-[27%] sm:ml-[35%]" />
                 </FormItem>
               )}
             />
@@ -128,10 +150,10 @@ export function AddActivityDialog({
               name="category"
               render={({ field }) => (
                 <FormItem className="flex flex-col">
-                  <div className="grid grid-cols-4 md:grid-cols-3 items-center gap-4">
+                  <div className="grid grid-cols-4 sm:grid-cols-3 items-center gap-4">
                     <FormLabel className="text-right">Category</FormLabel>
                     <Select onValueChange={field.onChange}>
-                      <FormControl className="col-span-3 md:col-span-2">
+                      <FormControl className="col-span-3 sm:col-span-2">
                         <SelectTrigger>
                           <SelectValue placeholder="Select a category" />
                         </SelectTrigger>
@@ -146,12 +168,75 @@ export function AddActivityDialog({
                       </SelectContent>
                     </Select>
                   </div>
-                  <FormMessage className="mt-1 ml-[25%] md:ml-[33%]" />
+                  <FormMessage className="mt-1 ml-[27%] sm:ml-[35%]" />
                 </FormItem>
               )}
             />
+            {exercises.map((exercise, index) => (
+              <div
+                key={`exercise-${index}`}
+                className="grid grid-cols-1 border-t-[2px] gap-2 pt-4"
+              >
+                {exerciseFields.map((exerciseField) => (
+                  <FormField
+                    key={`exercises.${index}.${exerciseField.name}`}
+                    control={form.control}
+                    name={
+                      `exercises.${index}.${exerciseField.name}` as
+                        | `exercises.${number}.name`
+                        | `exercises.${number}.sets`
+                        | `exercises.${number}.reps`
+                        | `exercises.${number}.weight`
+                    }
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <div className="grid grid-cols-4 sm:grid-cols-3 items-center gap-4">
+                          <FormLabel className="text-right">
+                            {exerciseField.label}
+                          </FormLabel>
+                          <FormControl className="col-span-3 sm:col-span-2">
+                            <Input
+                              type={
+                                exerciseField.name === "name"
+                                  ? "text"
+                                  : "number"
+                              }
+                              placeholder={exerciseField.placeholder}
+                              {...field}
+                              onChange={(e) => {
+                                const value =
+                                  exerciseField.name === "name"
+                                    ? e.target.value
+                                    : e.target.valueAsNumber;
+                                field.onChange(value);
+                              }}
+                            />
+                          </FormControl>
+                        </div>
+                        <FormMessage className="mt-1 ml-[27%] sm:ml-[35%]" />
+                      </FormItem>
+                    )}
+                  />
+                ))}
+              </div>
+            ))}
+
             <DialogFooter className="gap-2 sm:gap-0">
-              <Button type="submit">Add Exercise</Button>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  const newExercise: Exercise = {
+                    name: "",
+                    sets: 0,
+                    reps: 0,
+                    weight: 0,
+                  };
+                  form.setValue(`exercises.${exercises.length}`, newExercise);
+                  setExercises([...exercises, newExercise]);
+                }}
+              >
+                Add Exercise
+              </Button>
               <Button type="submit">Save</Button>
             </DialogFooter>
           </form>

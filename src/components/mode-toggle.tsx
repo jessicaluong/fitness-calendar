43267bc1b81ce877b1e9946f280597757907993e
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-08-29 12:29:03 -0700

    Fix light/dark theme mode toggle so toggle is on the button instead of on the icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,18 +3,16 @@ import { Button } from "@/components/ui/button";
 import { useTheme } from "@/contexts/theme-provider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <Button variant="ghost" size="rounded">
-      <Moon
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        onClick={() => setTheme("dark")}
-      />
-      <Sun
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        onClick={() => setTheme("light")}
-      />
+    <Button variant="ghost" size="rounded" onClick={toggleTheme}>
+      <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
